01c6e8b770ef56f6fd8b6e50eeb832aeb1066525
Fumihiro Ito <fmhrit@gmail.com>
2023-07-17 03:09:25 +0900

    markdown: Implement renderer

---
+++ markdown/renderer_test.go
@@ -0,0 +1,88 @@
+package markdown
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"go.f110.dev/notion-api/v3"
+)
+
+func TestRender(t *testing.T) {
+	buf, err := Render([]*notion.Block{
+		{
+			Type: notion.BlockTypeHeading2,
+			Heading2: &notion.Heading{
+				RichText: []*notion.RichTextObject{
+					{
+						Type: notion.RichTextObjectTypeText,
+						Text: &notion.Text{
+							Content: "Summary",
+						},
+						PlainText: "Summary",
+					},
+				},
+			},
+		},
+		{
+			Type: notion.BlockTypeParagraph,
+			Paragraph: &notion.Paragraph{
+				RichText: []*notion.RichTextObject{
+					{
+						Type: notion.RichTextObjectTypeText,
+						Text: &notion.Text{
+							Content: "Foobar",
+						},
+						PlainText: "Foobar",
+					},
+				},
+			},
+		},
+		{
+			Type: notion.BlockTypeCode,
+			Code: &notion.Code{
+				RichText: []*notion.RichTextObject{
+					{
+						Type: notion.RichTextObjectTypeText,
+						Text: &notion.Text{
+							Content: "$ uptime",
+						},
+						PlainText: "$ uptime",
+					},
+				},
+				Language: "console",
+			},
+		},
+		{
+			Type: notion.BlockTypeBulletedListItem,
+			BulletedListItem: &notion.Paragraph{
+				RichText: []*notion.RichTextObject{
+					{
+						Type: notion.RichTextObjectTypeText,
+						Text: &notion.Text{
+							Content: "Baz",
+						},
+						PlainText: "Baz",
+					},
+				},
+			},
+		},
+		{
+			Type: notion.BlockTypeBulletedListItem,
+			BulletedListItem: &notion.Paragraph{
+				RichText: []*notion.RichTextObject{
+					{
+						Type: notion.RichTextObjectTypeText,
+						Text: &notion.Text{
+							Content: "Qux",
+						},
+						PlainText: "Qux",
+					},
+				},
+			},
+		},
+	})
+	require.NoError(t, err)
+	assert.Equal(t, "## Summary\n\nFoobar\n\n```console\n$ uptime\n```\n\n- Baz\n- Qux\n", string(buf))
+}
